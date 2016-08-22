ad694635171d0315746bb8f088c8149a25b31940
LarryStanley <kill.xmurderer@gmail.com>
2016-08-23 03:24:20 +0800

    Update.

--- index.js
+++ index.js
@@ -114,18 +114,24 @@ const actions = {
 	},sendGreetings({context, entities}) {
 		return new Promise(function(resolve, reject) {
 				var results = [
-						"您好，我是中大美食機器人\n 我不叫Siri，很高興為您服務",
+						"您好，我是中大美食機器人 Abgal\n 我不叫Siri，很高興為您服務",
 						"安安，你好",
 						"您好，我是中大美食機器人",
 						"安安，給虧嗎？",
 						"您好，請問有什麼可以為您服務的嗎？",
 						"請問需要什麼服務嗎？",
-						"你好！！很高興問您服務"
+						"你好！！很高興為您服務"
 					]
 		        context.greeting = _.sample(results);
 			return resolve(context);
 	    });
-	},
+	},getMenu({context, entities}) {
+		return new Promise(function(resolve, reject) {
+				
+		        context.restaurant_url = "http://www.ncufood.info/" + firstEntityValue(entities, 'restaurant_name');
+			return resolve(context);
+	    });
+	}
 };
 
 const wit = new Wit({
