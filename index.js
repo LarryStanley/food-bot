6e75c9476a61bb30dcdb060dbc0c757e2355bfec
LarryStanley <kill.xmurderer@gmail.com>
2016-08-25 22:40:48 +0800

    update.

--- index.js
+++ index.js
@@ -209,7 +209,16 @@ const actions = {
 		return new Promise(function(resolve, reject) {
 			request('http://api.openweathermap.org/data/2.5/weather?lat=24.968129&lon=121.192645&units=metric&APPID=' + process.env.WEATHER_KEY, function(error, response, body) {
 				var result = JSON.parse(body);
-				context.temperature =  "\n" + result.main.temp + " 度";
+				if (context.temperature)
+					context.temperature = "\n攝氏" + result.main.temp ;
+				/*generics = true;
+				context.elements = [];
+				context.elements.push({
+					"subtitle": result.main.temp + "\n" + result.weather[0].main,
+					"title": "中央天氣",
+					"image_url": "http://c8.staticflickr.com/2/1601/24973800335_4a8f7bdcbd_k.jpg"
+				});*/
+
 				return resolve(context);
 			});
 	    });
