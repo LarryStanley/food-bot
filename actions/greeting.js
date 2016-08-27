21efb1988b0e7bc27803f65afae0878c42533eee
LarryStanley <kill.xmurderer@gmail.com>
2016-08-27 19:35:12 +0800

    Add greeting sentence.

--- actions/greeting.js
+++ actions/greeting.js
@@ -35,16 +35,32 @@ exports.sendGreetings = function({context, entities}) {
 exports.replyThanks = function({context, entities}) {
 	return new Promise(function(resolve, reject) {
 		var results = [
-		"不客氣～",
-		"有禮貌的小孩最乖了",
-		"這麼有禮貌，一定可以 All pass",
-		"不客氣，我給你87分不能再高了",
-		"能夠為您服務是我的榮幸",
-		"您太客氣了",
-		"不會不會",
-		"有禮貌的小孩好棒棒"
+			"不客氣～",
+			"有禮貌的小孩最乖了",
+			"這麼有禮貌，一定可以 All pass",
+			"不客氣，我給你87分不能再高了",
+			"能夠為您服務是我的榮幸",
+			"您太客氣了",
+			"不會不會",
+			"有禮貌的小孩好棒棒"
 		]
 		context.gratitude = _.sample(results);
+		return resolve(context);
+	});
+}
+
+exports.replyInsult = function({context, entities}) {
+	return new Promise(function(resolve, reject) {
+		var results = [
+			"你這樣下修央大的水準對嗎？",
+			"靠北，我沒跟你收錢就不錯了",
+			"中央的校訓不是誠樸嗎？這樣罵髒話對嗎？",
+			"實在是很抱歉，如果有招待不周可以好好說嘛？",
+			"阿呦，不要因為騎不到小綠就罵髒話啊",
+			"真的很抱歉，我才剛出生不久，智商有點低，招待不周",
+			"央大素質不意外"
+		]
+		context.reply = _.sample(results);
 		return resolve(context);
 	});
 }
