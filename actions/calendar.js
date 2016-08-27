66523eece823bbc50ddf8d9bc02bb0737511d284
LarryStanley <kill.xmurderer@gmail.com>
2016-08-27 19:34:48 +0800

    Fix async bugs.

--- actions/calendar.js
+++ actions/calendar.js
@@ -6,6 +6,7 @@ var rp = require('request-promise');
 var ical = require('ical');
 var Promise = require('promise');
 var _ = require('underscore');
+var async = require('async');
 
 const firstEntityValue = (entities, entity) => {
 	const val = entities && entities[entity] &&
@@ -22,38 +23,37 @@ exports.getCalendar = function({context, entities}) {
 	return new Promise(function(resolve, reject) {
 		ical.fromURL('https://calendar.google.com/calendar/ical/ncu.acad@gmail.com/public/basic.ics', {}, function(err, data) {
 			var entityEventName = firstEntityValue(entities, 'cal_event');
-			return new Promise(function(resolve, reject) {
-				var result = [];
-				_.each(data, function(value, key) {
-					const similarity = stringSimilarity.compareTwoStrings(value.summary, firstEntityValue(entities, 'cal_event'));
-					const timeDiff = moment(value.start).diff(moment());
-					if (similarity > 0.5 && timeDiff > 0) {
-						value.similarity = {
-							time: timeDiff,
-							text: stringSimilarity.compareTwoStrings(value.summary, firstEntityValue(entities, 'cal_event'))
-						};
-						result.push(value);
-					}
-				});
-				return resolve(result);
-			}).then(function(result) {
+			var result = [];
+			var index = 0;
+			async.each(data, function(value, callback){
+				index++;
+				const similarity = stringSimilarity.compareTwoStrings(value.summary, firstEntityValue(entities, 'cal_event'));
+				const timeDiff = moment(value.start).diff(moment());
+				if (similarity > 0.2 && timeDiff > 0) {
+					value.similarity = {
+						time: timeDiff,
+						text: stringSimilarity.compareTwoStrings(value.summary, firstEntityValue(entities, 'cal_event'))
+					};
+					result.push(value);
+				}
+				callback();
+			}, function(err) {
 				if (result.length) {
 					result = _.min(result, function(value) {
 						return value.similarity.time;
 					});
-
-					console.log(result);
 
 					moment.locale('zh-tw');
 					if (result.end && moment(result.start).format != moment(result.end))
 						context.calResult = "\n" + result.summary + "\n" + moment(result.start).format('ll') + " 到 " + moment(result.end).format('ll');
 					else
 						context.calResult = "\n" + result.summary + "\n" + moment(result.start).format('ll');
+					delete context.notFound;
 				} else {
 					context.notFound = true;
+					delete context.calResult;
 				}
 
-			}).then(function() {
 				return resolve(context);
 			});
 		});
